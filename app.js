e1b5a61ad0ab5bb218592636d36cad9e4ba3882e
Carlos Pedro <carlospedr@gmail.com>
2020-05-09 17:41:32 +0200

    RESP APi with Mongoose Part I

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const dishRouter = require('./routes/dishRouter')
 const leaderRouter = require('./routes/leaderRouter')
 const promoRouter = require('./routes/promoRouter')
+const Dishes = require('./models/dishes')
+
+
+const url = 'mongodb://localhost:27017/conFusion'
+mongoose.connect
+    .then(db => console.log('Connected to server'))
+    .catch((err) => console.log(err))
+
 
 var app = express();
 
@@ -29,19 +38,19 @@ app.use('/leaders', leaderRouter);
 app.use('/promos', promoRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -2,6 +2,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const dishRouter = express.Router()
+const moongoose = require('mongoose')
+
+const Dishes = require('../models/dishes')
 
 dishRouter.use(bodyParser.json())
 
@@ -13,37 +16,85 @@ dishRouter
         next()
     })
     .get((req, res, next) => {
-        res.end('Will send all dishes to you!')
+        return Dishes.find({}).exec()
+            .then(dishes => {
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'text/json')
+                res.json(dishes)
+            },
+                err => next(err))
+            .catch(err => next(err))
     })
     .post((req, res, next) => {
-        res.end('Will add the dish: ' +
-            req.body.name +
-            ' with details: ' +
-            req.body.description)
+        return Dishes.create(req.body)
+            .then(dish => {
+                res.statusCode = 200
+                console.log('Dish Created', dish)
+                res.json(dish)
+
+            },
+                err => next(err))
+            .catch(err => next(err))
     })
     .put((req, res, next) => {
         res.statusCode = 403
         res.end('PUT operation not supported on /dishes')
     })
     .delete((req, res, next) => {
-        res.end('Deleting all dishes for you!')
+        Dishes.revove({})
+            .then(response => {
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'text/json')
+                res.json(response)
+            },
+                err => next(err))
+            .catch(err => next(err))
     })
 
 dishRouter
     .route('/:dishId')
     .get((req, res, next) => {
+        Dishes
+            .findById(req.params.dishId)
+            .then(dish => {
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'text/json')
+                res.json(dish)
+            },
+                err => next(err))
+            .catch(err => next(err))
+
+
         res.end('Will send details of dish: ' + req.params.dishId)
     })
     .post((req, res, next) => {
         res.end('POST operation not supported on /dishes/' + req.params.dishId)
     })
     .put((req, res, next) => {
+        Dishes.findByIdAndUpdate(req.params.dishId,
+            { $set: req.body }, { new: true })
+            .then(dish => {
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'text/json')
+                res.json(dish)
+            },
+                err => next(err))
+            .catch(err => next(err))
+
         res.write('Updating the dish: ' + req.params.dishId + '\n')
         res.end('Will update  ' + req.body.name +
             ' with details : ' + req.body.description)
     })
     .delete((req, res, next) => {
-        res.end('Deleting dish: ' + req.params.dishId)
+        Dishes.findByIdAndDelete(req.params.dishId,
+            { new: true })
+            .then(response => {
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'text/json')
+                res.json(response)
+            },
+                err => next(err))
+            .catch(err => next(err))
     })
 
 
