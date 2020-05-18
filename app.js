c9d756ce5f71eb1f593acd71a8f261da81b80f0e
Carlos Pedro <carlospedr@gmail.com>
2020-05-18 17:58:50 +0200

    add https support

--- app.js
+++ app.js
@@ -36,7 +36,14 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(passport.initialize())
 
+app.all('*', (req, res, next) =>{
 
+    if(req.secure){
+        return next()
+    }
+    
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url)
+})
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use(express.static(path.join(__dirname, 'public')));
